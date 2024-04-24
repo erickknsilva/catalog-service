5b64126c0db08859aaa2a37457db5eee802eabe2
erickknsilva <erick.nsilva100@gmail.com>
2024-04-24 04:00:37 -0300

    Todas funcionalidades do CRUD criada e funcionando com sucesso, exceções e docker-compose

--- src/main/java/com/erickWck/catalogservice/web/ProductController.java
+++ src/main/java/com/erickWck/catalogservice/web/ProductController.java
@@ -3,6 +3,7 @@ package com.erickWck.catalogservice.web;
 import com.erickWck.catalogservice.domain.entity.Product;
 import com.erickWck.catalogservice.mapper.ProductMapper;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,28 +19,34 @@ public class ProductController {
         this.productService = productService;
     }
 
+
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     public ProductMapper postSave(@Valid @RequestBody Product product) {
         return productService.addCatalog(product);
     }
 
 
     @GetMapping("/find/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ProductMapper findById(@PathVariable @Valid Long id) {
         return productService.viewDetailsProduct(id);
     }
 
     @GetMapping("/edit/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ProductMapper editProductById(@PathVariable @Valid Long id, @RequestBody @Valid Product product) {
         return productService.editDetailsProduct(id, product);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<ProductMapper> findAll() {
         return productService.listAllProduct();
     }
 
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeProduct(@PathVariable("id") Long id) {
         productService.remove(id);
     }
--- src/main/java/com/erickWck/catalogservice/web/ProdutctControlleAdvice.java
+++ src/main/java/com/erickWck/catalogservice/web/ProdutctControlleAdvice.java
@@ -2,21 +2,43 @@ package com.erickWck.catalogservice.web;
 
 import com.erickWck.catalogservice.web.exception.ProductAlreadyExist;
 import com.erickWck.catalogservice.web.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ProdutctControlleAdvice {
 
     @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     String productNotFoundHandler(ProductNotFoundException ex) {
         return ex.getMessage();
     }
 
     @ExceptionHandler(ProductAlreadyExist.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     String productAlreadExistsHandler(ProductAlreadyExist ex) {
         return ex.getMessage();
     }
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        var erros = new HashMap<String, String>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            erros.put(fieldName, errorMessage);
+        });
+        return erros;
+    }
 
 }
